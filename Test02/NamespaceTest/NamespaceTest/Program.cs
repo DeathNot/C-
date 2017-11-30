2c9d69023099409094462e9de4173e5947877bcb
DeathNot <725509787@qq.com>
2017-11-30 11:36:23 +0800

    namespace test

---
+++ Test02/NamespaceTest/NamespaceTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Li;
+
+namespace NamespaceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Qg oa = new Qg();
+            oa.Myls();
+        }
+    }
+}
+
+namespace Li
+{
+    class Qg
+    {
+        public void Myls()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
